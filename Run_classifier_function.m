e92d6cb2e714f0c1b40ab3fc8a2feabc91ac7c88
sophie-l-mason <111516243+sophie-l-mason@users.noreply.github.com>
2022-08-22 09:51:19 +0100

    Update Run_classifier_function.m

--- Run_classifier_function.m
+++ Run_classifier_function.m
@@ -7,10 +7,9 @@ contrast=1; %select 1 for [1,-1] or -1 for [-1,1]
 N_perms=1000; %Number of permutations used to create the p_val e.e. 1000 or 5000
 alpha=0.05; %significance threshold level
 path='C:\Users\sophi\Documents\NBS_github' %this is a path for which a folder is created results to be saved
-
+random_seed=1; % sets the seed used for the random seed generator 'shuffle' uses a different random seed each time
 % The classifier
-rng(1)%sets the seed used for the random seed generator 'shuffle' uses a different random seed each time
-[class_label,Accuracy,P_val, N_edges,Step]=Classifier_function(Data,Design_matrix,contrast,N_perms,alpha,path)
+[class_label,Accuracy,P_val, N_edges,Step]=Classifier_function(Data,Design_matrix,contrast,N_perms,alpha,path,random_seed)
 
 %% Aditional inputs for the varying the t-statistic threshold in the classifier
 threshold_min=0.01; %minimum t-statistic threshold to consider
@@ -19,6 +18,5 @@ Title='Random data' %title for the figure
 Y_Label='[ECP $>$ LCP]' %y label for the figure
 figure_index=1 %the number for figure window  
 % The classifier: optimised for varying the t-statistic threshold
-rng(1)%sets the seed used for the random seed generator 'shuffle' uses a different random seed each time
-[Results,class_label]=NBS_vary_t_stat(Data,Design_matrix,contrast,N_perms,alpha,path,threshold_min,threshold_max,Title,Y_Label,figure_index)
+[Results,class_label]=NBS_vary_t_stat(Data,Design_matrix,contrast,N_perms,alpha,path,threshold_min,threshold_max,Title,Y_Label,figure_index,random_seed)
 
